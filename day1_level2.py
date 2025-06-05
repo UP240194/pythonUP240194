3f0777a3333080268e2265839839aac330d8d68b
UP240194 <up240194@alumnos.upa.edu.mx>
2025-06-05 10:37:40 -0600

    creacion del primer archivo para el repositorio (day1-2)

---
+++ day1_level2.py
@@ -0,0 +1,42 @@
+#Exercise: level 2
+
+print(3+2)
+print(3-2)
+print(3*2)
+print(3%2)
+print(3/2)
+print(3**2)
+print(3//2)
+print("Ximena")
+print("Leo")
+print("Ags")
+print("Im enjoying 30 days of python")
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh','python','finland']))
+print("Ximena")
+print(type(['ana','alberto','leo','helena']))
+print("Ags")
+
+#Exercise:Level 3
+#1)
+
+print(2)
+print(3.4)
+print(1+g)
+print("I love my pet")
+print(true)
+print(type(['It','He','She']))
+print('mila','coffe','hanzo')
+print(2,4,1)
+
+#2)
+
+print('distancia eclaudina')
+p1= (2,3)
+p2= (10,8)
+suma_de_cuadrados_=((p2[0]-p1[0])**2)+((p2[1]-p1)**2)
+distancia= (suma_de_cuadrados_)**0.5
+print('la distancia eclaudiana de los puntos',p1, 'y',p2,round(distancia,2))
---
+++ day_2.py
@@ -0,0 +1,14 @@
+first_name=input('¿Cual es tu nombre?')
+last_name=input('¿Cual es tu primer apellido?')
+full_name=('Ximena Ruiz')
+country=('¿Cual es tu pais de procedencia?')
+city='Ags'
+age=input('¿Cual es tu edad?')
+year=2006
+is_married,is_true,is_light_on=True,True,True
+
+#Checking data types
+print('Data types d')
+print(type(first_name))
+print(type(last_name))
+print(type())
